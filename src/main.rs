f4b2e77f423ef746709e39c68993dc08ed7d13c3
Florian Grima <florian.grima@epitech.eu>
2025-05-18 17:45:11 -0400

    feat: adding vga buffer

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
 #![no_std] // ne pas lier la bibliothèque standard Rust
 #![no_main] // désactiver tous les points d'entrée au niveau de Rust
-
+mod vga_buffer;
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
@@ -22,5 +14,6 @@ pub extern "C" fn _start() -> ! {
 /// Cette fonction est appelée à chaque panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println_err!("{}", _info);
     loop {}
 }
